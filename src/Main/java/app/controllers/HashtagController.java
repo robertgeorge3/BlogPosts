34062b3d1978b474102ad222de2cb93d2115a74f
robertgeorge3 <robertgeorge717@gmail.com>
2022-02-27 18:38:57 +0000

    Index HTMl made

--- src/Main/java/app/controllers/HashtagController.java
+++ src/Main/java/app/controllers/HashtagController.java
@@ -4,12 +4,15 @@ package app.controllers;
 import app.dao.HashtagRepository;
 import app.dto.Hashtags;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
+@RequestMapping
 public class HashtagController {
     @Autowired
     HashtagRepository hashtagDao;
--- src/Main/java/app/controllers/PostController.java
+++ src/Main/java/app/controllers/PostController.java
@@ -3,24 +3,27 @@ package app.controllers;
 import app.dao.PostRepository;
 import app.dto.Posts;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@RestController
+@Controller
+@RequestMapping
 public class PostController {
     @Autowired
     PostRepository postRepository;
+
+    @RequestMapping("/")
+    public String begin(){
+        return "Index.html";
+    }
 
     @GetMapping("/Test")
     public List<Posts> findall(){
         return postRepository.findAll();
     }
-
-
 
     @GetMapping("/Test2")
     public Posts findID(){
